2d714e0d55ac5a9fa327652d5877b3aa8c0c1bc4
Shashwat Goel <shashwatnow@gmail.com>
2024-10-30 18:36:13 +0100

    scripts for running multiple datasets

--- w2s/mpi_scripts/run.sh
+++ w2s/mpi_scripts/run.sh
@@ -2,8 +2,8 @@ source ~/miniforge3/etc/profile.d/conda.sh # equivalent to conda init
 conda activate diff
 export HOME=/home/sgoel
 # optionally parse args
-arg_1=$1
-arg_2=$2
+arg_1="$1"
+arg_2="$2"
 
 # execute python script
-python run.py --dataset=$arg_1 --run_name=$arg_2
+python run.py --dataset="$arg_1" --run_name="$arg_2"
